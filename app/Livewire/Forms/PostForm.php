72cc5ffa4c896be243a9b4150eddca30970e9e57
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2025-02-20 15:37:18 +0700

    finished -5

---
+++ app/Livewire/Forms/PostForm.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Livewire\Forms;
+
+use Livewire\Form;
+use App\Models\User;
+use Livewire\Attributes\Rule;
+use Livewire\Attributes\Validate;
+
+class PostForm extends Form
+{
+    #[Rule(['required', 'string', 'min:3', 'max:255'])]
+    public string $title = '';
+    
+    #[Rule(['required'])]
+    public string $body = '';
+
+    public function store(): void 
+    {
+        $user = User::find(1);
+        
+        $user->posts()->create(
+            $this->validate()
+        );
+        
+
+        $this->reset();
+    }
+}
--- app/Livewire/Posts/Create.php
+++ app/Livewire/Posts/Create.php
@@ -2,27 +2,20 @@
 
 namespace App\Livewire\Posts;
 
+use App\Livewire\Forms\PostForm;
 use App\Models\User;
 use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class Create extends Component
-{
-    #[Rule(['required', 'string', 'min:3', 'max:255'])]
-    public string $title = '';
+{   
+    public PostForm $form;
     
-    #[Rule(['required'])]
-    public string $body = '';
-    
-    public function save()
+    public function save(): void
     {
-        $user = User::find(1);
-        $validated = $this->validate();
-
-        $user->posts()->create($validated);
-
-        $this->reset();
+        $this->form->store();
     }
+    
     public function render()
     {
         return view('livewire.posts.create');
